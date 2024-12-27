1e0b9b695beec74312ee57e53a0bb03ba9fdea1e
no-naame <145124868+no-naame@users.noreply.github.com>
2024-12-27 10:54:34 +0530

    solved multipart

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],
+    allow_origins=["https://chat-with-pdf-three-black.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
